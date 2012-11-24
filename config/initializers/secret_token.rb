1a45898bc296fb920c87d8edc4a03bf9c62d61bb
Neville Samuell <nevilles@gmail.com>
2012-11-24 17:49:15 -0500

    Adding the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookDemo::Application.config.secret_token = 'e839e3b4728609e76224a3d8d7e7b12254ff650295c484dc71d87847e556574d05cc4af2111ffdf0e6b195392275ed666329ee73fac14065162511e496057c96'
