c1249aa30bf6e6089c71edf76d97526e433862d0
Neville Samuell <nevilles@gmail.com>
2012-11-25 12:36:27 -0500

    Removing everything and starting over. This is overly complicated

--- app/models/authentication.rb
+++
@@ -1,4 +0,0 @@
-class Authentication < ActiveRecord::Base
-  belongs_to :user
-  attr_accessible :provider, :token, :uid, :user_id
-end
--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-# Configure the Omniauth gem
-Rails.application.config.middleware.use OmniAuth::Builder do
-  # Configure Facebook app & permissions
-  provider :facebook, "525862300758263", "cffd79debd10d6fc847925abc77262e5", {:scope => 'email, user_photos, friends_photos, publish_stream, offline_access'}
-end
